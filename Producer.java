6b9907a385bf035a7e380463698fcf88f6c38042
Dimitrisgian1 <30804541+Dimitrisgian1@users.noreply.github.com>
2020-03-04 12:12:54 +0200

    Update Producer.java

--- Producer.java
+++ Producer.java
@@ -1,26 +1,39 @@
 package lab1;
+import java.util.concurrent.*; 
 
-public class Producer {
+public class Producer extends Thread{
 
-	static Good good;
-	static boolean check;
-	
-	public Producer(boolean c)
+	public Producer(Semaphore sem)
 	{
-        good.str = "abc";
-        good.i = 0;
-        check = c;
+	    public void run()
+	    {
+	    	Good prodgood = Producer.produce();
+	        Producer.put(prodgood, sem);
+	        return;
+	    }
+	}
+
+	public static Good produce()
+	{
+		return new Good("abc", 1);
 	}
 	
 	
-	public static void put()
+	public static void put(Good good, Semaphore sem)
 	{
-
+		while(true)
+		{
+			try {
+				sem.acquire();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}			
 			//Critical sector
 			Storage.put(good);
 			//end critical sector
-			
-
+			sem.release();			
+			//remaining section
+			System.out.println("Running Producer remaining sector...\nFinished producer remaining sector!");
+		}
 	}
 }
-
