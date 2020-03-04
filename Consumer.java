73f9efeed7fa8faeffd663fcd049d47f262dfae2
Dimitrisgian1 <30804541+Dimitrisgian1@users.noreply.github.com>
2020-03-04 12:12:18 +0200

    Update Consumer.java

--- Consumer.java
+++ Consumer.java
@@ -1,12 +1,40 @@
 package lab1;
+import java.util.concurrent.*; 
 
-public class Consumer {
+public class Consumer extends Thread{
 
-	public static Good get()
+	public Consumer(Semaphore sem)
 	{
-		//Critical sector
-		return Storage.get();
-		//end critical sector
+		Thread cons = new Thread()
+		{
+		    public void run()
+		    {		        
+		    	Good consgood = Consumer.get(sem);
+		    	if(!(consgood.str == "abc" && consgood.i == 1))
+		    		System.out.println("Error: Corrupted Good!\n");
+		    	return;
+		    }
+		};
+	}
+	
+	public static Good get(Semaphore sem)
+	{
+		while(true)
+		{
+			try {
+				sem.acquire();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			//Critical sector
+			Good good = Storage.get();
+			//end critical sector
+			sem.release();
+			System.out.println("Running consumer remaining sector...\nFinished consumer remaining sector!");
+
+			return good;
+		}
+
 	}
 }
-
